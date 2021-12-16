f380ae1d56dda5667cc578093fc25cd385811d26
Rasedul Islam <rasedulsaju@gmail.com>
2021-12-16 17:34:58 +0600

    Database Connectivity
    Database Files

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+    $dbserver="localhost"; // Database Server or Host
+    $dbuser="root"; // Database User
+    $dbpass=""; // Database User's Password
+    $db="networkerbahire"; // Database Name
+    $dbport="3306"; // Database Port
+?>
