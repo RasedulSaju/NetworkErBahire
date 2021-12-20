5eee09f9a3c48bcb4e8e8abdda3823b5158110f2
Rasedul Islam <rasedulsaju@gmail.com>
2021-12-20 17:37:58 +0600

    extension handled & 404 added
    added .htaccess for extension handling & 404 error page added.

---
+++ 404.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<html>
+
+<head>
+    <!-- meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Google Site Verification --
+	<meta name="google-site-verification" content="" />-->
+
+    <title>Requested file is Network Er Bahire</title>
+	
+	<!-- Favicon -->
+    <link href="https://lh3.googleusercontent.com/iFWXv9xOSeC3olDCVOFQKySe4lba2-XPgsux4EfbewgMQEU_Q3RuC_q6BqV5UXYBUAchE_3a_l46=w1202-h901-no" rel="icon"/>
+	
+    <!-- bootstrap CSS file link -->
+    <!-- <link rel="stylesheet" type="text/css" href="https://www.rasedulsaju.com/css/bootstrap.min.css">-->
+    <!-- Custom CSS file load -->
+    <!-- Custom CSS -->
+    <!-- <link rel="stylesheet" type="text/css" href="https://www.rasedulsaju.com/css/style.css">-->
+	<!-- 404 CSS -->
+	<link rel="stylesheet" type="text/css" href="https://www.rasedulsaju.com/css/404.css">
+    <!-- mdb footer -->
+    <!-- <link rel="stylesheet" type="text/css" href="https://www.rasedulsaju.com/css/mdb.css">-->
+    <!-- font-awesome CSS load -->
+    <!-- <link rel="stylesheet" type="text/css" href="https://www.rasedulsaju.com/font-awesome/css/font-awesome.min.css">-->
+</head>
+
+<body>
+	
+	<!-- 404 Body -->
+	<div class="error-page">
+		<div>
+			<!--h1(data-h1='400') 400-->
+			<!--p(data-p='BAD REQUEST') BAD REQUEST-->
+			<!--h1(data-h1='401') 401-->
+			<!--p(data-p='UNAUTHORIZED') UNAUTHORIZED-->
+			<!--h1(data-h1='403') 403-->
+			<!--p(data-p='FORBIDDEN') FORBIDDEN-->
+			<h1 data-h1="File is">File is</h1>
+			<p data-p="Network Er Bahire">Network Er Bahire</p>
+			<!--h1(data-h1='500') 500-->
+			<!--p(data-p='SERVER ERROR') SERVER ERROR-->
+		</div>
+	</div>
+	<div id="particles-js"></div>
+	<!-- /. 404 Body -->
+	
+	
+    <!-- JavaScripts -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	
+    <!--<script src="https://www.rasedulsaju.com/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://www.rasedulsaju.com/js/popper.min.js"></script>
+    <script src="https://www.rasedulsaju.com/js/bootstrap.min.js"></script>-->
+	
+	<!-- 404 Script -->
+	<script src="https://www.rasedulsaju.com/js/404.js"></script>
+	
+    <!-- MDB Footer JS -->
+    <!--<script type="text/javascript" src="https://www.rasedulsaju.com/js/mdb.min.js"></script>-->
+</body>
+
+</html>
