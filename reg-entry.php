7f879200f1219621854ef1e44888350818e9a87b
rakibulislam7253 <altabrakib7@gmail.com>
2021-12-28 00:10:03 +0600

    Update reg-entry.php

--- reg-entry.php
+++ reg-entry.php
@@ -12,14 +12,14 @@ if (
 
     $var1 = $_POST['email'];
     $var2 = $_POST['pass']; //md5($_POST['pass']);
-echo"i am rakib";
+
     try {
         ///php-mysql 3 way. We will use PDO - PHP data object
         $dbcon = new PDO("mysql:host=$dbserver:$dbport;dbname=$db;", "$dbuser", "$dbpass");
         $dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         $sqlquery = "Insert into users (email, password) values ('$var1', '$var2')";
-        echo $sqlquery;
+        //echo $sqlquery;
         try {
 
             $returnval = $dbcon->query($sqlquery);
